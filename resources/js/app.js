329f0747d40030f41067b9da282f3c157096807b
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-12-14 20:22:57 +0700

    Create Vue Board page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+require("./bootstrap");
+
+window.Vue = require("vue");
+
+Vue.component("board", require("./components/Board.vue").default);
+
+const app = new Vue({
+    el: "#app",
+});
