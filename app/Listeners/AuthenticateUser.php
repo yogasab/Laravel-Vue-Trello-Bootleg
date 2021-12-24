828dedd0d7823a5dd4d9d35f9ddb35fb21425e29
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-12-24 21:51:15 +0700

    Create listener for authenticate user register

---
+++ app/Listeners/AuthenticateUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticateUser
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $guard = Auth::guard('web');
+        $guard->login($event->user);
+    }
+}
